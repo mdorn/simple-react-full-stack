a2ce8553e2856d26281c42ed563f2d709f4cb8c4
Matt Dorn <matt.dorn@gmail.com>
2018-10-23 08:11:20 -0500

    Update various config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,41 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+require('dotenv').config();
 
 const outputDirectory = 'dist';
 
 module.exports = {
+  // entry: `${__dirname}/src/client/index.js`,
   entry: './src/client/index.js',
   output: {
     path: path.join(__dirname, outputDirectory),
     filename: 'bundle.js'
   },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.(css|scss)$/,
+        use: [
+          // fallback to style-loader in development
+          // process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   },
@@ -33,7 +47,16 @@ module.exports = {
     }
   },
   plugins: [
+    new webpack.DefinePlugin({
+       // TODO: whitelist env vars for use on client side
+      'process.env': JSON.stringify({
+        'API_URL': process.env.API_URL,
+      })
+    }),
     new CleanWebpackPlugin([outputDirectory]),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/favicon.ico'
